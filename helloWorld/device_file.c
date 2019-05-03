6c5d383be86c28520d7a6b45e44a04da45738485
Sarah <sarahfchastanet@gmail.com>
2019-05-03 13:39:28 -0400

    ft-hello_world: Updates to driver so that it builds
      - Simplify the Makefile
      - Add comments into device_file.c and call 'raw_copy_to_user'
      - Add license and developer information to hello_world.c

--- helloWorld/device_file.c
+++ helloWorld/device_file.c
@@ -13,9 +13,15 @@
 #include <linux/module.h>
 #include <asm/uaccess.h>
 
+// Define static global variables used to register the device
+static int device_file_major_number = 0;
+static const char device_name[] = "Simple-driver";
 static const char g_s_hello_world_string[] = "Hello world from kernel mode!\n\0";
 static const ssize_t g_s_hello_world_size = sizeof(g_s_hello_world_string);
 
+//*********************************************************************
+
+// read the device file
 static ssize_t device_file_read(struct file *file_ptr,
 								char __user *user_buffer,
 								size_t count,
@@ -23,42 +29,40 @@ static ssize_t device_file_read(struct file *file_ptr,
 {
 	printk(KERN_NOTICE "Simple-driver: Device file is read at offset = %i, read bytes count = %u", (int)*position, (unsigned int) count);
 
+	// if position is behind the end of a file, we have nothing to read
 	if(*position >= g_s_hello_world_size) {
 		return 0;
 	}
 
+	// if a user tries to read more than we have, read only as many bytes as we have
 	if(*position + count > g_s_hello_world_size) {
 		count = g_s_hello_world_size - *position;
 	}
 
-	if(copy_to_user(user_buffer, g_s_hello_world_string + *position, count) != 0) {
+	// copy_to_user declared in asm/uaccess.h. Function simply copies specific
+	// data from the kernel buffer to the buffer allocated in the user space.
+	if(raw_copy_to_user(user_buffer, g_s_hello_world_string + *position, count) != 0) {
 		return -EFAULT;
 	}
 
+	// move reading position
 	*position += count;
 	return count;
 }
 
-// Register a character  device
-int register_chrdev(unsigned int major,
-					const char * name,
-					const struct fops);
-					file_operations *
+//*********************************************************************
 
-// Device file operations (read, write, save) are processsed by
-// the function pointers stored within the file_operations structure.
-// As we are ensuring the operation of only a single type of
-// device with our Linux driver, our file_operations structure
-// is global and static.
+/* Device file operations (read, write, save) are processsed by
+ * the function pointers stored within the file_operations structure.
+ * As we are ensuring the operation of only a single type of
+ * device with our Linux driver, our file_operations structure
+ * is global and static.
+ */
 static struct file_operations simple_driver_fops =
 {
 	.owner = THIS_MODULE,
 	.read = device_file_read,
 };
-
-static int device_file_major_number = 0;
-static const char device_name[] = "Simple-driver";
-//*********************************************************************
 
 // register the device
 int register_device(void)
--- helloWorld/hello_world.c
+++ helloWorld/hello_world.c
@@ -6,8 +6,20 @@
  */
 
 #include "device_file.h"
-#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+/*
+ * Provide some basic information about the driver, and
+ * define a license so that this module can be loaded
+ */
+#define DRIVER_AUTHOR "Sarah Chastanet"
+#define DRIVER_DESC   "A sample driver"
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_SUPPORTED_DEVICE("testdevice");
 
 // load module
 static int __init simple_init(void)
